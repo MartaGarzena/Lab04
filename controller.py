008bb3ed552305b91c78a298b1f8fb1aba43bd38
marta <marta.garzena@gmail.com>
2025-03-18 14:57:57 +0100

    aggiunta row1,row2, row3 senza controlli

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ import time
 import flet as ft
 import model as md
 
+
 class SpellChecker:
 
     def __init__(self, view):
@@ -44,10 +45,40 @@ class SpellChecker:
             case _:
                 return None
 
+    def handleSpellCheck(self, e):
+        txtIno = self._view._txtIn.value
+        if txtIno == "":
+            self._view.txtOut.controls.clear()
+            self._view.txtOut.controls.append(ft.Text(value="Add a sentence!"))
+            return
+
+        language = self._view.dd.value
+        modality = self._view.dd_mode.value
+
+        if language == "":
+            self._view.txtOut.controls.clear()
+            self._view.txtOut.controls.append(ft.Text(value="Add a language!"))
+            return
+
+        if modality == "":
+            self._view.txtOut.controls.clear()
+            self._view.txtOut.controls.append(ft.Text(value="Add a modality!"))
+            return
+
+        paroleErrate, time = self.handleSentence(txtIno, language, modality)
+
+        if paroleErrate is not None:
+            self._view.txtOut.controls.append(ft.Text(value=paroleErrate + " in " + str(time) + " seconds"))
+
+        else:
+            self._view.txtOut.controls.append(ft.Text(value="No errors found in " + str(time) + " seconds"))
+
+        self._view._txtIn.value = ""
+        self._view.page.update()
 
     def printMenu(self):
         print("______________________________\n" +
-              "      SpellChecker 101\n"+
+              "      SpellChecker 101\n" +
               "______________________________\n " +
               "Seleziona la lingua desiderata\n"
               "1. Italiano\n" +
--- view.py
+++ view.py
@@ -1,8 +1,14 @@
 import flet as ft
 
+
 class View(object):
     def __init__(self, page: ft.Page):
         # Page
+        self.txtOut = None
+        self._btnIn = None
+        self.dd_mode = None
+        self._txtIn = None
+        self.dd = None
         self.page = page
         self.page.title = "TdP 2024 - Lab 04 - SpellChecker ++"
         self.page.horizontal_alignment = 'CENTER'
@@ -25,17 +31,33 @@ class View(object):
             ft.Row(spacing=30, controls=[self.__theme_switch, self.__title, ],
                    alignment=ft.MainAxisAlignment.START)
         )
-
         # Add your stuff here
+        self.dd = ft.Dropdown(label="Select Language",
+                              options=[ft.dropdown.Option("Italian"), ft.dropdown.Option("English"),
+                                       ft.dropdown.Option("Spanish")])
 
-        self.page.add([])
+        self.dd_mode = ft.Dropdown(label="Search Modality",
+                                   options=[ft.dropdown.Option("Linear"), ft.dropdown.Option("Dichotomic"),
+                                            ft.dropdown.Option("Default")])
 
+        self._txtIn = ft.TextField(label="Inserisci la tua frase")
+        self._btnIn = ft.ElevatedButton("Spell Check", on_click=self.__controller.handleSpellCheck)
+
+        self.txtOut = ft.ListView()
+
+        row1 = ft.Row([self.dd])
+        row2 = ft.Row([self.dd_mode, self._txtIn, self._btnIn])
+        row3 = ft.Row([self.txtOut])
+
+        self.page.add(row1, row2, row3)
         self.page.update()
 
     def update(self):
         self.page.update()
+
     def setController(self, controller):
         self.__controller = controller
+
     def theme_changed(self, e):
         """Function that changes the color theme of the app, when the corresponding
         switch is triggered"""
